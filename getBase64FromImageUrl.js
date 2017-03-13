0726e9628b78ff385ffe1bf76d081d4b2a39ade9
matikrk <matikrk@gmail.com>
2017-03-13 14:03:09 +0100

    Update and rename getBase64FromImageUrl to getBase64FromImageUrl.js
    Promise version

--- getBase64FromImageUrl.js
+++ getBase64FromImageUrl.js
@@ -1,4 +1,5 @@
-function getBase64FromImageUrl(url,callback) {
+function getBase64FromImageUrl(url) {
+    return new Promise(res => {
         const img = new Image();
 
         img.setAttribute('crossOrigin', 'anonymous');
@@ -12,10 +13,11 @@ function getBase64FromImageUrl(url,callback) {
             ctx.drawImage(this, 0, 0);
 
             const dataURL = canvas.toDataURL("image/png");
-            callback(dataURL);
+            res(dataURL);
         };
 
         img.src = url;
+    });
 };
 
 
